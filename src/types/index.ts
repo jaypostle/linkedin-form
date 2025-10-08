158b872fdd02bb58d060e69bd74fd6e9505ffc64
Jayson Postle <jaysondigitalmcc@gmail.com>
2025-10-08 09:45:40 -0700

    Adjust types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,16 +4,15 @@ export type JobExperience = {
   job_title: string;
   employment_type: (typeof employmentTypes)[number];
   company: string;
+  start_date: { month: string; year: string };
 };
 
 export type CurrentJobExperienceType = JobExperience & {
   is_current: true;
-  start_date: { month: string; year: string };
 };
 
 export type PreviousJobExperienceType = JobExperience & {
   is_current: false;
-  start_date: { month: string; year: string };
   end_date: { month: string; year: string };
 };
 
